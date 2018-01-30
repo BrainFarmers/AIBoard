5834c0c7ea295be9fbed4b3475db885b7c6b8d53
Skit5 <chapeau.plume@gmail.com>
2018-01-30 01:40:05 +0100

    Boost connected

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,63 @@
-//#include <boost/lambda/lambda.hpp>
-//#include <boost/regex.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/regex.hpp>
 #include <iostream>
 #include <string>
 #include <iterator>
 #include <algorithm>
+#include <boost/format.hpp>
+#include <boost/date_time.hpp>
+#include <boost/process.hpp>
 
 using namespace std;
-//using namespace boost::lambda;
+using namespace boost::lambda;
+using namespace boost::process;
 
-//typedef istream_iterator<int> in;
+typedef istream_iterator<int> in;
 
 int main()
 {
 
-    /*std::string line;
+    /// Load configurations
+
+    /// Launch runtime
+
+    ipstream pipe_stream;
+    child c("gcc --version", std_out > pipe_stream);
+
+    string line;
+
+    while (pipe_stream && getline(pipe_stream, line) && !line.empty())
+        cerr << line << endl;
+
+    c.wait();
+
+    /*const boost::posix_time::ptime now = boost::posix_time::second_clock::local_time();
+
+    const boost::wformat f = boost::wformat( L"%02d.%02d.%s  %02d:%02d" )
+                    % now.date().year_month_day().day.as_number()
+                    % now.date().year_month_day().month.as_number()
+                    % now.date().year_month_day().year
+                    % now.time_of_day().hours()
+                    % now.time_of_day().minutes();
+
+    std::wcout << f.str() << std::endl;*/
+
+    /*string line;
     boost::regex pat( "^Subject: (Re: |Aw: )*(.*)" );
 
-    while (std::cin)
+    while (cin)
     {
-        std::getline(std::cin, line);
+        getline(cin, line);
         boost::smatch matches;
         if (boost::regex_match(line, matches, pat))
-            std::cout << matches[2] << std::endl;
+            cout << matches[2] << endl;
     }*/
 
     //for_each(in(cin), in(), cout << (_1 * 3) << " " );
-
-    cout << "Hello world!" << endl;
+    /*cout << "Hello world!" << endl;
 
     string c;
-    cin >> c;
+    cin >> c;*/
 
     return 0;
 }
